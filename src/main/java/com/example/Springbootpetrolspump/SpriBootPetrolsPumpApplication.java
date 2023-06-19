7a98b897b3ffa1bd8f3d87d2dc242ab0c878a3e5
Shivakant6387 <shivakant5875@gmail.com>
2023-06-19 13:10:40 +0530

    nothing to commit

---
+++ src/main/java/com/example/Springbootpetrolspump/SpriBootPetrolsPumpApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springbootpetrolspump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpriBootPetrolsPumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpriBootPetrolsPumpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Springbootpetrolspump/SpriBootPetrolsPumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springbootpetrolspump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpriBootPetrolsPumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
